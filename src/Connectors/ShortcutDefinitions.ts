d51e092fbd03510aa25a571ffd53b67ae731496d
William O'Sullivan-Dargis <william19.od@gmail.com>
2021-01-09 17:25:15 -0500

    Added shortcut definitions

--- src/Connectors/ShortcutDefinitions.ts
+++ src/Connectors/ShortcutDefinitions.ts
@@ -1,26 +1,47 @@
 import { fsUtils } from './Utils';
 
+export interface IShortcutCategory {
+  label: string;
+  shortcuts: IShortcutDefinition[];
+}
+
 export interface IShortcutDefinition {
   id: string;
   label: string;
-  metaTags?: string[]
+  metaTags?: string[];
 }
 
 export class ShortcutDefinitions {
-  private static readonly DEFAULT_SC_DEFINITIONS_PATH =
-    'src/Connectors/Config/ShortcutDefinitions.json';
-
   public definitions: IShortcutDefinition[];
 
   constructor(definitions?: IShortcutDefinition[]) {
     this.definitions = definitions ?? [];
   }
 
+  public static async read(): Promise<ShortcutDefinitions> {
+    const categories = await ShortcutCategories.read();
+    const definitions = categories.categories.flatMap(
+      (category) => category.shortcuts
+    );
+    return new ShortcutDefinitions(definitions);
+  }
+}
+
+export class ShortcutCategories {
+  public static readonly DEFAULT_SC_DEFINITIONS_PATH =
+    'src/Connectors/Config/ShortcutDefinitions.json';
+
+  public categories: IShortcutCategory[];
+
+  constructor(categories?: IShortcutCategory[]) {
+    this.categories = categories ?? [];
+  }
+
   public static async read(
-    path: string = ShortcutDefinitions.DEFAULT_SC_DEFINITIONS_PATH
-  ): Promise<ShortcutDefinitions> {
-    return new ShortcutDefinitions(
-      await fsUtils.readJson<IShortcutDefinition[]>(path)
+    path: string = ShortcutCategories.DEFAULT_SC_DEFINITIONS_PATH
+  ): Promise<ShortcutCategories> {
+    return new ShortcutCategories(
+      await fsUtils.readJson<IShortcutCategory[]>(path)
     );
   }
 }
