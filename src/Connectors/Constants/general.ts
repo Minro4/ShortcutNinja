b7ae08a71cec2dc02ddffba3622226651e72484c
William O'Sullivan-Dargis <william19.od@gmail.com>
2021-01-13 01:58:42 -0500

    css

--- src/Connectors/Constants/general.ts
+++ src/Connectors/Constants/general.ts
@@ -1,8 +1,10 @@
-import * as path from "path";
+import * as path from 'path';
 
-export const APP_NAME = "keymap-manager";
+export const APP_NAME = 'keymap-manager';
 export const CONFIG_PATH = path.join(__dirname, 'Connectors/Config');
-export const IDE_MAPPINGS_PATH = path.join(CONFIG_PATH, "ideMappings");
+export const IDE_MAPPINGS_PATH = path.join(CONFIG_PATH, 'ideMappings');
 
-export const SHORTCUT_DEFINITIONS_PATH = path.join(CONFIG_PATH,'ShortcutDefinitions.json');
-
+export const SHORTCUT_DEFINITIONS_PATH = path.join(
+  CONFIG_PATH,
+  'ShortcutDefinitions.json'
+);
